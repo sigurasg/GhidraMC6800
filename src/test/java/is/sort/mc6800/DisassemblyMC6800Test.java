87e6028c7e263436c3fff4ff0b1dd5e01639ff92
Sigurdur Asgeirsson <siggi@sort.is>
2025-12-11 19:35:18 +0000

    Refactor mc6800 valid/invalid opcode test.

--- src/test/java/is/sort/mc6800/DisassemblyMC6800Test.java
+++ src/test/java/is/sort/mc6800/DisassemblyMC6800Test.java
@@ -22,81 +22,28 @@ public class DisassemblyMC6800Test extends DisassemblyCommonTest {
 	}
 
 	@Test
-	public void InvalidOpCodes() {
-		assertInvalidOpcode(0x00);
-		assertInvalidOpcode(0x02);
-		assertInvalidOpcode(0x03);
-		assertInvalidOpcode(0x04);
-		assertInvalidOpcode(0x05);
+	public void ValidInvalidOpCodes() {
+		Integer[] invalidOpcodes = {
+			0x00, 0x02, 0x03, 0x04, 0x05,
+			0x12, 0x13, 0x14, 0x15, 0x18,
+			0x1A, 0x1C, 0x1D, 0x1E, 0x1F,
+			0x21,
+			0x38, 0x3A, 0x3C, 0x3D,
+			0x41, 0x42, 0x45, 0x4B, 0x4E,
+			0x51, 0x52, 0x55, 0x5B, 0x5E,
+			0x61, 0x62, 0x65, 0x6B,
+			0x71, 0x72, 0x75, 0x7B,
+			0x83, 0x87, 0x8F,
+			0x93, 0x9D,
+			0xA3,
+			0xB3,
+			0xC3, 0xC7, 0xCC, 0xCD, 0xCF,
+			0xD3, 0xDC, 0xDD,
+			0xE3, 0xEC, 0xED,
+			0xF3, 0xFC, 0xFD
+		};
 
-		assertInvalidOpcode(0x12);
-		assertInvalidOpcode(0x13);
-		assertInvalidOpcode(0x14);
-		assertInvalidOpcode(0x15);
-		assertInvalidOpcode(0x18);
-
-		assertInvalidOpcode(0x1A);
-		assertInvalidOpcode(0x1C);
-		assertInvalidOpcode(0x1D);
-		assertInvalidOpcode(0x1E);
-		assertInvalidOpcode(0x1F);
-
-		assertInvalidOpcode(0x21);
-
-		assertInvalidOpcode(0x38);
-		assertInvalidOpcode(0x3A);
-		assertInvalidOpcode(0x3C);
-		assertInvalidOpcode(0x3D);
-
-		assertInvalidOpcode(0x41);
-		assertInvalidOpcode(0x42);
-		assertInvalidOpcode(0x45);
-		assertInvalidOpcode(0x4B);
-		assertInvalidOpcode(0x4E);
-
-		assertInvalidOpcode(0x51);
-		assertInvalidOpcode(0x52);
-		assertInvalidOpcode(0x55);
-		assertInvalidOpcode(0x5B);
-		assertInvalidOpcode(0x5E);
-
-		assertInvalidOpcode(0x61);
-		assertInvalidOpcode(0x62);
-		assertInvalidOpcode(0x65);
-		assertInvalidOpcode(0x6B);
-
-		assertInvalidOpcode(0x71);
-		assertInvalidOpcode(0x72);
-		assertInvalidOpcode(0x75);
-		assertInvalidOpcode(0x7B);
-
-		assertInvalidOpcode(0x83);
-		assertInvalidOpcode(0x87);
-		assertInvalidOpcode(0x8F);
-
-		assertInvalidOpcode(0x93);
-		assertInvalidOpcode(0x9D);
-
-		assertInvalidOpcode(0xA3);
-
-		assertInvalidOpcode(0xB3);
-
-		assertInvalidOpcode(0xC3);
-		assertInvalidOpcode(0xC7);
-		assertInvalidOpcode(0xCC);
-		assertInvalidOpcode(0xCD);
-		assertInvalidOpcode(0xCF);
-
-		assertInvalidOpcode(0xD3);
-		assertInvalidOpcode(0xDC);
-		assertInvalidOpcode(0xDD);
-
-		assertInvalidOpcode(0xE3);
-		assertInvalidOpcode(0xEC);
-		assertInvalidOpcode(0xED);
-
-		assertInvalidOpcode(0xF3);
-		assertInvalidOpcode(0xFC);
-		assertInvalidOpcode(0xFD);
+		assertInvaldOpcodes(invalidOpcodes);
+		assertValidOpcodes(complementOpcodes(invalidOpcodes));
 	}
 }
